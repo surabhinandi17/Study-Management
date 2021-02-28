14b6edb765f7374d656aba95b767be0de2fd1224
Surabhi <surabhinandi17@gamil.com>
2021-02-28 22:04:26 +0530

    this is my second commit

--- dashboard.php
+++ dashboard.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php require('links.php'); ?>
-    <title> Dashboard </title>
+    <title> Study  </title>
     <style type="text/css">
         @media only screen and (max-width: 800px) {
             .if{
